94fdf64cdfd492fdbcf287603f9afc3c806825e2
falt <falt.andreas@gmail.com>
2018-02-02 12:59:35 +0100

    add mock data table

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 import bomb from './bomb.png';
 import './App.css';
+import Table from'./Table.js'
 
 class App extends Component {
   render() {
@@ -12,6 +13,11 @@ class App extends Component {
           <img src={bomb} className="App-logo" alt="logo" />
           <h1 className="App-title">SURVIVAL OF THE FASTEST</h1>
         </header>
+        <Table />
+        <Table />
+        <Table />
+        <Table />
+        <Table />
         <p className="App-intro">
           <a href="https://orange-dev.duckdns.org:8080/"><code>START GAME</code></a>
         </p>
